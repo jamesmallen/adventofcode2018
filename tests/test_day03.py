e310e51eb96766f5f7d9b773db7e3e21ba37d0a8
James M. Allen <james.m.allen@gmail.com>
2018-12-03 23:31:03 -0500

    Day 3

---
+++ tests/test_day03.py
@@ -0,0 +1,28 @@
+from typing import Mapping
+
+import pytest
+
+from adventofcode2018 import day03
+
+
+@pytest.mark.parametrize('x,output', [
+    ('''
+    #1 @ 1,3: 4x4
+    #2 @ 3,1: 4x4
+    #3 @ 5,5: 2x2
+    ''', 4),
+])
+def test_part1(x: str, output: int) -> None:
+    assert day03.part1(x) == output
+
+
+@pytest.mark.parametrize('x,output', [
+    ('''
+    #1 @ 1,3: 4x4
+    #2 @ 3,1: 4x4
+    #3 @ 5,5: 2x2
+    ''', 3),
+])
+def test_part2(x: str, output: int) -> None:
+    assert day03.part2(x) == output
+
