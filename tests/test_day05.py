c2431b6be1a931065d5506d9472a327da750a3e6
James Allen <james.m.allen@gmail.com>
2019-01-16 10:54:24 -0500

    Woops, extra space

--- tests/test_day05.py
+++ tests/test_day05.py
@@ -5,10 +5,12 @@ from adventofcode2018 import day05
 
 @pytest.mark.parametrize('x,output', [
     ('aA', 0),
+    ('aa', 2),
     ('abBA', 0),
     ('abAB', 4),
     ('aabAAB', 6),
     ('dabAcCaCBAcCcaDA', 10),
+    ('xabcCdDBA', 1),
 ])
 def test_part1(x: str, output: int) -> None:
     assert day05.part1(x) == output
